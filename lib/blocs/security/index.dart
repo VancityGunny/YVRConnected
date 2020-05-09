d641b7adcbb52416467c43b3132b1d0005c1ded2
Gunny <vancitygunny@gmail.com>
2020-05-08 21:44:08 -0700

    show top 5 friends

--- lib/blocs/security/index.dart
+++
@@ -1,8 +0,0 @@
-export 'security_bloc.dart';
-export 'security_event.dart';
-export 'security_model.dart';
-export 'security_page.dart';
-export 'security_provider.dart';
-export 'security_repository.dart';
-export 'security_screen.dart';
-export 'security_state.dart';
--- lib/ui/home_latest_widget.dart
+++ lib/ui/home_latest_widget.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:yvrconnected/blocs/friend/friend_bloc.dart';
 import 'package:yvrconnected/blocs/friend/friend_event.dart';
+import 'package:yvrconnected/blocs/friend/friend_stat_model.dart';
 import 'package:yvrconnected/blocs/thought/index.dart';
 
 // Widget to show all latest thoughts received
@@ -16,7 +17,7 @@ class HomeLatestWidget extends StatefulWidget {
 
 class HomeLatestWidgetState extends State<StatefulWidget> {
   List<ThoughtModel> latestThoughts = List<ThoughtModel>();
-
+  List<FriendStatModel> topFiveFriends = List<FriendStatModel>();
   @override
   void initState() {
     super.initState();
@@ -29,34 +30,61 @@ class HomeLatestWidgetState extends State<StatefulWidget> {
         latestThoughts = result;
       });
     });
-    thoughtProvider.fetchTopFive();
+    thoughtProvider.fetchTopFive().then((result) {
+      // If we need to rebuild the widget with the resulting data,
+      // make sure to use `setState`
+      setState(() {
+        topFiveFriends = result;
+      });
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-          itemCount: latestThoughts.length,
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 3,
+    return Column(
+      children: <Widget>[
+        Expanded(
+          child: Container(
+            child: GridView.builder(
+              itemCount: latestThoughts.length,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+              ),
+              itemBuilder: (context, index) {
+                return GestureDetector(
+                    onLongPress: () {
+                      //TODO: open the message
+                    }, // open action option, miss, remind, grateful
+                    onLongPressUp: () {
+                      //TODO: selectActionOption, // long press release so select whatever was selected
+                    },
+                    child: Card(
+                      child: Column(
+                        children: <Widget>[
+                          Container(child: Icon(Icons.email), height: 80),
+                          Text(latestThoughts[index].thoughtOptionCode,
+                              style: TextStyle(color: Colors.deepPurple))
+                        ],
+                      ),
+                    ));
+              },
+            ),
           ),
-          itemBuilder: (context, index) {
-            return GestureDetector(
-                onLongPress: () {
-                  //TODO: open the message
-                }, // open action option, miss, remind, grateful
-                onLongPressUp: () {
-                  //TODO: selectActionOption, // long press release so select whatever was selected
-                },
-                child: Card(
-                  child: Column(
-                    children: <Widget>[
-                      Container(child: Icon(Icons.email), height: 80),
-                      Text(latestThoughts[index].thoughtOptionCode,
-                          style: TextStyle(color: Colors.deepPurple))
-                    ],
-                  ),
-                ));
-          },
-        );
+        ),
+        Expanded(
+            child: Container(
+          child: ListView.builder(
+              itemCount: topFiveFriends.length,
+              itemBuilder: (BuildContext ctxt, int index) {
+                return ListTile(
+                  leading: Icon(Icons.favorite),
+                  title: Text(topFiveFriends[index].friend.displayName +
+                      ':' +
+                      topFiveFriends[index].thoughtSent.toString()),
+                );
+              }),
+        ))
+      ],
+    );
   }
 }
